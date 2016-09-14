56001f09bee1e6457d99e2b69682fe742f04b313
Salome Eriksson <salome.eriksson@unibas.ch>
2016-09-14 17:22:25 +0200

    certificate output correct var order, fixed m&s bug (forgot to add neg vars)

--- src/unsolvability/certificate.h
+++ src/unsolvability/certificate.h
@@ -14,20 +14,11 @@
  * Note that each certificate contains ALL variables from the task (even those which
  * were not present when creating the certificate), and they also have a primed version
  * of each variable right next to the unprimed one.
- * original_bdd_vars denotes the number of variables used when creating the certificate
- * (without their primed version)
- *
- * Example: Task has variable order <a,b,c,d,e>; the certificate had at creation time
- * variable order <c,b,d>
- * After reading in the certificate, the variable order is as follows:
- * <c,c',b,b',d,d',a,a',e,e'>
- * original_bdd_vars = 3
  */
 
 class Certificate {
 protected:
   std::vector<int> fact_to_bddvar;
-  int original_bdd_vars;
   Task* task;
   Cudd manager;
   // also builds the mapping between global facts and bdd variables
