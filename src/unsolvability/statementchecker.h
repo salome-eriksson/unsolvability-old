eab51d72455846f165b9f308b3b1ba3c45be246a
Salome Eriksson <salome.eriksson@unibas.ch>
2017-09-04 18:30:58 +0200

    renamed files from cpp to cc and moved some code from header to source

--- src/unsolvability/statementchecker.h
+++ src/unsolvability/statementchecker.h
@@ -20,35 +20,9 @@ protected:
     KnowledgeBase *kb;
     Task *task;
     // TODO: this is a duplicate from RuleChecker::determine_parameters
-    std::vector<std::string> determine_parameters(const std::string &parameter_line) {
-        std::vector<std::string> tokens;
-        size_t prev = 0, pos = 0;
-        do
-        {
-            pos = parameter_line.find(";", prev);
-            if (pos == std::string::npos) {
-                pos = parameter_line.length();
-            }
-            std::string token = parameter_line.substr(prev, pos-prev);
-            if (!token.empty()) tokens.push_back(token);
-            prev = pos + delim.length();
-        }
-        while (pos < parameter_line.length() && prev < parameter_line.length());
-        return tokens;
-    }
-
+    std::vector<std::string> determine_parameters(const std::string &parameter_line);
     // TOOD: this is a duplicate from RuleChecker::parseCube)
-    Cube RuleChecker::parseCube(const std::string &param) {
-        Cube cube;
-        cube.reserve(task->get_number_of_facts());
-        std::istringstream iss(param);
-        int n;
-        while (iss >> n){
-            cube.push_back(n);
-        }
-        assert(cube.size() == task->get_number_of_facts());
-        return cube;
-    }
+    Cube parseCube(const std::string &param);
 
 public:
     StatementChecker(KnowledgeBase *kb, Task *task) : kb(kb), task(task) {}
