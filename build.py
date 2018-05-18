e683b26477bc2798d314419daef20a778e88b4c6
Salome Eriksson <salome.eriksson@unibas.ch>
2018-05-18 14:51:10 +0200

    added option to build verifier in build.py

--- build.py
+++ build.py
@@ -43,7 +43,7 @@ def print_usage():
     generator_name = CMAKE_GENERATOR.lower()
     default_config_name = DEFAULT_CONFIG_NAME
     debug_config_name = DEBUG_CONFIG_NAME
-    print("""Usage: {script_name} [BUILD [BUILD ...]] [--all] [--debug] [MAKE_OPTIONS]
+    print("""Usage: {script_name} [BUILD [BUILD ...]] [--all] [--debug] [--build-verifier] [MAKE_OPTIONS]
 
 Build one or more predefined build configurations of Fast Downward. Each build
 uses {cmake_name} to generate {generator_name} and then uses {make_name} to compile the
@@ -52,9 +52,10 @@ code. Build configurations differ in the parameters they pass to {cmake_name}.
 Build configurations
   {configs_string}
 
---all         Alias to build all build configurations.
---debug       Alias to build the default debug build configuration.
---help        Print this message and exit.
+--all            Alias to build all build configurations.
+--debug          Alias to build the default debug build configuration.
+--build-verifier Build verifier for all configurations.
+--help           Print this message and exit.
 
 Make options
   All other parameters are forwarded to {make_name}.
@@ -86,7 +87,7 @@ def get_build_path(config_name):
     return os.path.join(get_builds_path(), config_name)
 
 
-def build(config_name, cmake_parameters, make_parameters):
+def build(config_name, cmake_parameters, make_parameters, build_verifier):
     print("Building configuration " + config_name)
     build_path = get_build_path(config_name)
     rel_src_path = os.path.relpath(get_src_path(), build_path)
@@ -103,12 +104,20 @@ def build(config_name, cmake_parameters, make_parameters):
                           + [rel_src_path],
                           cwd=build_path)
     subprocess.check_call([MAKE] + make_parameters, cwd=build_path)
+    
+    if(build_verifier):
+        print("Building verifier")
+        verify_path = os.path.join(get_project_root_path(), "src/unsolvability")
+        subprocess.check_call([MAKE], cwd=verify_path)
+        subprocess.check_call(["cp", os.path.join(os.path.relpath(verify_path, build_path), "verify"), "bin"], cwd=build_path)
+    
     print("Built configuration " + config_name + " successfully")
 
 
 def main():
     config_names = set()
     make_parameters = []
+    build_verifier=False;
     for arg in sys.argv[1:]:
         if arg == "--help" or arg == "-h":
             print_usage()
@@ -119,12 +128,14 @@ def main():
             config_names |= set(CONFIGS.keys())
         elif arg in CONFIGS:
             config_names.add(arg)
+        elif arg == "--build-verifier":
+            build_verifier=True;            
         else:
             make_parameters.append(arg)
     if not config_names:
         config_names.add(DEFAULT_CONFIG_NAME)
     for config_name in config_names:
-        build(config_name, CONFIGS[config_name], make_parameters)
+        build(config_name, CONFIGS[config_name], make_parameters, build_verifier)
 
 
 if __name__ == "__main__":
