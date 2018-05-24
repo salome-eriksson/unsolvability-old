67618a219998de2435af1ae630327c7ea04a7e81
Salome Eriksson <salome.eriksson@unibas.ch>
2018-05-24 11:00:06 +0200

    faster max certificate generation (build bdd from bottom instead of top)

--- src/search/merge_and_shrink/merge_and_shrink_representation.h
+++ src/search/merge_and_shrink/merge_and_shrink_representation.h
@@ -3,6 +3,7 @@
 
 #include <memory>
 #include <vector>
+#include <unordered_map>
 
 #include "../unsolvability/cudd_interface.h"
 
@@ -28,8 +29,9 @@ public:
     virtual void apply_abstraction_to_lookup_table(
         const std::vector<int> &abstraction_mapping) = 0;
     virtual void dump() const = 0;
-    virtual void get_unsolvability_certificate(CuddBDD* h_inf,
-                std::vector<CuddBDD> &bdd_for_val, bool fill_bdd_for_val) = 0;
+    virtual void get_bdds(CuddManager *manager, std::unordered_map<int, CuddBDD> &bdd_for_val) = 0;
+    virtual CuddBDD* get_unsolvability_certificate(
+                CuddManager *manager, std::unordered_map<int, CuddBDD> &bdd_for_val, bool first) = 0;
 };
 
 
@@ -46,8 +48,9 @@ public:
         const std::vector<int> &abstraction_mapping) override;
     virtual int get_value(const State &state) const override;
     virtual void dump() const override;
-    virtual void get_unsolvability_certificate(CuddBDD* h_inf,
-                std::vector<CuddBDD> &bdd_for_val, bool fill_bdd_for_val);
+    virtual void get_bdds(CuddManager *manager, std::unordered_map<int, CuddBDD> &bdd_for_val);
+    virtual CuddBDD* get_unsolvability_certificate(
+                CuddManager *manager, std::unordered_map<int, CuddBDD> &bdd_for_val, bool first);
 };
 
 
@@ -66,8 +69,9 @@ public:
         const std::vector<int> &abstraction_mapping) override;
     virtual int get_value(const State &state) const override;
     virtual void dump() const override;
-    virtual void get_unsolvability_certificate(CuddBDD* h_inf,
-                std::vector<CuddBDD> &bdd_for_val, bool fill_bdd_for_val);
+    virtual void get_bdds(CuddManager *manager, std::unordered_map<int, CuddBDD> &bdd_for_val);
+    virtual CuddBDD* get_unsolvability_certificate(
+                CuddManager *manager, std::unordered_map<int, CuddBDD> &bdd_for_val, bool first);
 };
 }
 
