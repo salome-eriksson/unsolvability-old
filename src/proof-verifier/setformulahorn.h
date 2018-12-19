153f9c8b8f91e22249e67b3317bf2c84049987d8
Salome Eriksson <salome.eriksson@unibas.ch>
2018-12-19 17:01:06 +0100

    rework setformulahorn (work in progress)

--- src/proof-verifier/setformulahorn.h
+++ src/proof-verifier/setformulahorn.h
@@ -8,6 +8,16 @@
 
 class SetFormulaHorn;
 
+struct HornConjunctionElement {
+    const SetFormulaHorn *formula;
+    const bool primed;
+    std::vector<int> removed_implications;
+
+    HornConjunctionElement (SetFormulaHorn *formula, bool primed)
+        : formula(formula), primed(primed) {
+    }
+};
+
 typedef std::vector<std::pair<const SetFormulaHorn *,bool>> HornFormulaList;
 typedef std::vector<std::pair<std::unordered_set<int>,std::unordered_set<int>>> VariableOccurence;
 
@@ -23,9 +33,52 @@ private:
     HornUtil(Task *task);
 
     void build_actionformulas();
+    bool get_horn_vector(std::vector<SetFormula *> &formulas, std::vector<SetFormulaHorn *> &horn_formulas);
+
+    /*
+     * Perform unit propagation through the conjunction of the formulas, returning whether it is satisfiable or not.
+     * Partial assignment serves both as input of a partial assignment, and as output indicating which variables
+     * are forced true/false by the conjunction.
+     */
+    bool simplify_conjunction(std::vector<HornConjunctionElement> &conjuncts, Cube &partial_assignment);
+    bool is_restricted_satisfiable(SetFormulaHorn *formula, Cube &restriction);
+
+    bool conjunction_implies_disjunction(std::vector<HornConjunctionElement> &conjuncts, std::vector<SetFormulaHorn *> &disjuncts);
+
+    /*bool is_satisfiable(const HornFormulaList &formulas, bool partial = false);
+    bool is_satisfiable(const HornFormulaList &formulas, Cube &solution, bool partial = false);
+    bool is_restricted_satisfiable(const HornFormulaList &formulas, const Cube &restrictions, bool partial = false);
+    // restriction is a partial assignment to the variables (0 = false, 1 = true, 2 = dont care).
+    // Any content in solution will get overwritten (even if the formula is not satisfiable).
+    // If partial is true, then the solution will return 0 for forced false vars, 1 for forced true, and
+    // 2 for others. A concrete solution would be to set all don't cares to false
+    bool is_restricted_satisfiable(const HornFormulaList &formulas, const Cube &restrictions,
+                                   Cube &solution, bool partial= false);
+    bool implies(const HornFormulaList &formulas,
+                        const SetFormulaHorn *right, bool right_primed);
+    bool implies_union(const HornFormulaList &formulas,
+                                      const SetFormulaHorn *union_left, bool left_primed,
+                                      const SetFormulaHorn *union_right, bool right_primed);
+    bool implies_union_helper(const HornFormulaList &formulas, Cube &restrictions,
+                              const SetFormulaHorn *right_formula, int right_offset,
+                              const Cube &formulas_solution_partial);
+                              */
 };
 
 
+/*
+ * A Horn Formula consist of the following parts:
+ *  - forced true: represents all unit clauses with one positive literal
+ *  - forced false: represents all unit clauses with one negative literal
+ *  - All nonunit clauses are represented by left_vars and right_side:
+ *    - left_vars[i] is the negative literals of clause [i]
+ *    - right_side is the positive literal (or -1 if none exists) of clause [i]
+ *
+ * left_sizes and variable_occurences are helper infos for checking satisfiability
+ *   - left_sizes stores for each clause how many negative literals it contains
+ *   - variable occurences stores for each variable, in which clause it appears
+ *     (the first unordered_set is for negative occurence, the second for positive)
+ */
 class SetFormulaHorn : public SetFormulaBasic
 {
     friend class HornUtil;
@@ -40,9 +93,15 @@ private:
 
     static HornUtil *util;
 
+    /*
+     * Private constructors can only be called by SetFormulaHorn or HornUtil,
+     * meaning the static member SetFormulaHorn::util is already initialized
+     * and we thus do not need to worry about initializing it.
+     */
     // this constructor is used for setting up the formulas in util
     SetFormulaHorn(const std::vector<std::pair<std::vector<int>,int>> &clauses, int varamount);
-    // this method should be called from every constructor!
+    // used for getting a simplified conjunction of several (possibly primed) formulas
+    SetFormulaHorn(std::vector<HornConjunctionElement> &elements);
     void simplify();
 public:
     // TODO: this is currently only used for a dummy initialization
@@ -56,26 +115,12 @@ public:
     const std::vector<int> &get_left_sizes() const;
     const std::vector<int> &get_left_vars(int index) const;
     int get_right(int index) const;
+    const std::vector<int> &get_right_sides() const;
     const std::unordered_set<int> &get_variable_occurence_left(int var) const;
     const std::unordered_set<int> &get_variable_occurence_right(int var) const;
     const std::vector<int> & get_forced_true() const;
     const std::vector<int> & get_forced_false() const;
     void dump() const;
-
-    static bool is_satisfiable(const HornFormulaList &formulas, bool partial = false);
-    static bool is_satisfiable(const HornFormulaList &formulas, Cube &solution, bool partial = false);
-    static bool is_restricted_satisfiable(const HornFormulaList &formulas, const Cube &restrictions, bool partial = false);
-    // restriction is a partial assignment to the variables (0 = false, 1 = true, 2 = dont care).
-    // Any content in solution will get overwritten (even if the formula is not satisfiable).
-    // If partial is true, then the solution will return 0 for forced false vars, 1 for forced true, and
-    // 2 for others. A concrete solution would be to set all don't cares to false
-    static bool is_restricted_satisfiable(const HornFormulaList &formulas, const Cube &restrictions,
-                                   Cube &solution, bool partial= false);
-    static bool implies(const HornFormulaList &formulas,
-                        const SetFormulaHorn *right, bool right_primed);
-    static bool implies_union(const HornFormulaList &formulas,
-                                      const SetFormulaHorn *union_left, bool left_primed,
-                                      const SetFormulaHorn *union_right, bool right_primed);
 
     virtual bool is_subset(std::vector<SetFormula *> &left,
                            std::vector<SetFormula *> &right);
